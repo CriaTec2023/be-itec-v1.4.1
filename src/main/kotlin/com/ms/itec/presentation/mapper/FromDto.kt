6defc1436b68ac8b26e94fbcca17e1c490161f92
Luis Felipe da Silva Mota de Souza <116108026+Lupesms97@users.noreply.github.com>
2024-01-05 17:57:38 -0300

    feat: creation of mapper for ProspectModel from Dto

--- src/main/kotlin/com/ms/itec/presentation/mapper/FromDto.kt
+++ src/main/kotlin/com/ms/itec/presentation/mapper/FromDto.kt
@@ -1,8 +1,11 @@
 package com.ms.itec.presentation.mapper
 
 import com.ms.itec.application.dto.request.ContentDto
+import com.ms.itec.application.dto.request.ProspectModelDto
+import com.ms.itec.application.enums.Polos
 import com.ms.itec.application.enums.Tag
 import com.ms.itec.entity.Content
+import com.ms.itec.domain.entity.ProspectModel
 import jakarta.validation.Valid
 import java.util.UUID
 
@@ -18,5 +21,22 @@ class FromDto {
             tag = Tag.valueOf(dto.tag),
             avgSalary = dto.avgSalary
         )
-    }}
+    }
+
+    fun toEntity(@Valid dto: ProspectModelDto): ProspectModel {
+
+        return ProspectModel(
+            id = UUID.randomUUID().toString(),
+            name = dto.name,
+            email = dto.email,
+            phone = dto.phone,
+            polo = if (dto.polo != null ) Polos.valueOf(dto.polo!!) else Polos.UNDECIDED,
+            course = dto.course,
+            cupom = dto.cupom.toString(),
+            emailMarketing = dto.emailMarketing,
+            contacted = false,
+            ownerId = ""
+        )
+    }
+}
 
