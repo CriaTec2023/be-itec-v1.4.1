5f286a95f0744f0e24d5aff1369f846af48fbae5
Lupesms97 <lupesms97@gmail.com>
2024-01-07 13:41:44 -0300

    feat: adding methods for data handling

--- src/main/kotlin/com/ms/itec/presentation/mapper/FromDto.kt
+++ src/main/kotlin/com/ms/itec/presentation/mapper/FromDto.kt
@@ -18,25 +18,44 @@ class FromDto {
             description = dto.description,
             content = dto.content,
             background = dto.background,
-            tag = Tag.valueOf(dto.tag),
+            tag = Tag.valueOf(dto.tag.trim()),
             avgSalary = dto.avgSalary
         )
     }
 
     fun toEntity(@Valid dto: ProspectModelDto): ProspectModel {
 
+
+
         return ProspectModel(
             id = UUID.randomUUID().toString(),
-            name = dto.name,
-            email = dto.email,
-            phone = dto.phone,
-            polo = if (dto.polo != null ) Polos.valueOf(dto.polo!!) else Polos.UNDECIDED,
-            course = dto.course,
+            name = dto.name.trim(),
+            email = dto.email.trim(),
+            phone = formatBrazilianPhoneNumber(dto.phone),
+            polo = if (dto.polo != null ) Polos.valueOf(dto.polo!!.trim()) else Polos.UNDECIDED,
+            course = dto.course.trim(),
             cupom = dto.cupom.toString(),
             emailMarketing = dto.emailMarketing,
             contacted = false,
             ownerId = ""
         )
+    }
+
+    fun formatBrazilianPhoneNumber(input: String): String {
+        // Remove todos os caracteres não numéricos
+        val cleanedInput = input.replace("[^\\d]".toRegex(), "")
+
+        // Verifica se o número tem o formato esperado (DDD + 9 dígitos)
+        return if (cleanedInput.length == 11) {
+            // Extrai o DDD e o número de telefone
+            val ddd = cleanedInput.substring(0, 2)
+            val phoneNumber = cleanedInput.substring(2)
+
+            // Formata o número
+            "($ddd) $phoneNumber"
+        } else {
+            "Número inválido"
+        }
     }
 }
 
