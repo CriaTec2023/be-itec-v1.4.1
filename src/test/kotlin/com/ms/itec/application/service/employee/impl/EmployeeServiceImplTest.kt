896fac85a6af4c24389e113f553cccc3d7055f31
Luis Felipe da Silva Mota de Souza <116108026+Lupesms97@users.noreply.github.com>
2024-02-16 09:23:12 -0300

    update: createdAt field for EmployeeModel Class

---
+++ src/test/kotlin/com/ms/itec/application/service/employee/impl/EmployeeServiceImplTest.kt
@@ -0,0 +1,101 @@
+package com.ms.itec.application.service.employee.impl
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class EmployeeServiceImplTest {
+
+    @Test
+    fun saveEmployeeModel0() {
+    }
+
+    @Test
+    fun deleteEmployeeModelById0() {
+    }
+
+    @Test
+    fun findEmployeeModelById0() {
+    }
+
+    @Test
+    fun updateEmployeeModel0() {
+    }
+
+    @Test
+    fun getAllEmployeeModels0() {
+    }
+
+    @Test
+    fun findAll0() {
+    }
+    @Test
+    fun saveEmployeeModel1() {
+    }
+
+    @Test
+    fun deleteEmployeeModelById1() {
+    }
+
+    @Test
+    fun findEmployeeModelById1() {
+    }
+
+    @Test
+    fun updateEmployeeModel1() {
+    }
+
+    @Test
+    fun getAllEmployeeModels1() {
+    }
+
+    @Test
+    fun findAll1() {
+    }
+    @Test
+    fun saveEmployeeModel2() {
+    }
+
+    @Test
+    fun deleteEmployeeModelById2() {
+    }
+
+    @Test
+    fun findEmployeeModelById2() {
+    }
+
+    @Test
+    fun updateEmployeeModel2() {
+    }
+
+    @Test
+    fun getAllEmployeeModels2() {
+    }
+
+    @Test
+    fun findAll2() {
+    }
+    @Test
+    fun saveEmployeeModel() {
+    }
+
+    @Test
+    fun deleteEmployeeModelById() {
+    }
+
+    @Test
+    fun findEmployeeModelById() {
+    }
+
+    @Test
+    fun updateEmployeeModel() {
+    }
+
+    @Test
+    fun getAllEmployeeModels() {
+    }
+
+    @Test
+    fun findAll() {
+    }
+}
